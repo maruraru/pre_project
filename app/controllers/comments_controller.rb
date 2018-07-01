620dc6ba0b8691bf44db611ec46e0e06c3de196d
Avir <qliray@yandex.ru>
2018-07-02 01:34:27 +0300

    Raitings: ok, username: ok, rooms: not working (nested_form issue, wtf)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -27,6 +27,6 @@ class CommentsController < ApplicationController
  
   private
     def comment_params
-      params.require(:comment).permit(:comment)
+      params.require(:comment).permit(:comment, :localrate)
     end
 end
