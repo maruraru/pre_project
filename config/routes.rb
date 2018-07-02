1bb5f2c546779f08b210ee3747838b8c61fcb39e
khaetskaya <anastasia_khaetskaya18@mail.ru>
2018-07-02 19:33:01 +0300

    Added header in layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 	get 'home/index'
+	get '/devise/hotels', to: 'hotels#index'
 	resources :hotels do
 		resource :location
 		resources :comments, :rooms
