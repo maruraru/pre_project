aff48a2797cdb8a7fd28c2fbaca7473e6ee14763
Avir <qliray@yandex.ru>
2018-07-02 15:33:21 +0300

    Now edit should work

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,13 +1,13 @@
 class Hotel < ApplicationRecord
-	has_one :location
-	has_many :comments
+	has_one :location, dependent: :destroy
+	has_many :comments, dependent: :destroy
 
-	has_many :rooms
+	has_many :rooms, dependent: :destroy
 
 	mount_uploader :photo, PhotoUploader
 	validates :title, presence: true, length: {in: 5..20}, uniqueness: true
 	validates :photo, presence: true
-	accepts_nested_attributes_for :location
+	accepts_nested_attributes_for :location, allow_destroy: true
 	
 	accepts_nested_attributes_for :rooms, allow_destroy: true
 
