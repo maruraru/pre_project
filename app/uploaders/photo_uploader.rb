8adbda75c1d670b79700727b825e612157137b4a
Maya <meari.kachusova@mail.ru>
2018-07-03 17:22:00 +0300

    resize is working fine now

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -10,7 +10,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
 
   version :thumb do
-    process resize_to_fill:[200,200]
+    process :resize_to_fill => [400,200]
   end
     
   version :full do
