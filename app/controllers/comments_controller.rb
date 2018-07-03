97d2c9a4447f513510d9b8dd07b7e69eb37445ce
Avir <qliray@yandex.ru>
2018-07-04 00:54:05 +0300

    need to fix delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,31 +2,34 @@ class CommentsController < ApplicationController
 
   before_action :authenticate_user!
 
+  before_action :get_curr_hotel, only: [:create,:destroy]
+  
   def new
   	@comment = Comment.new
   end
 
   def create
-    @hotel = Hotel.find(params[:hotel_id])
+    #@hotel = Hotel.find(params[:hotel_id])
     @comment = current_user.comments.build(comment_params)
     @comment.hotel_id = @hotel.id
     redirect_to hotel_path(@hotel) if @comment.save
   end
 
   def destroy
+    #@hotel = Hotel.find(params[:hotel_id])
+    @comment = @hotel.comments.find(params[:id])
+    @comment.destroy
+    redirect_to hotel_path(@hotel)
   end
-
-
-  # def create
-   # @hotel = Hotel.find(params[:article_id])
-   # @comment = @hotel.comments.create(comment_params)
-   # current_user.comment=@comment
-   # comment
-   # redirect_to hotel_path(@hotel)
-  #end
  
   private
     def comment_params
       params.require(:comment).permit(:comment, :localrate)
     end
+
+
+    def get_curr_hotel 
+      @hotel = Hotel.find(params[:hotel_id])
+    end
+
 end
