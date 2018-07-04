f2d313f833a67ce8b01071e075ae1edff87ec17a
Avir <qliray@yandex.ru>
2018-07-04 04:03:29 +0300

    Rooms: limited success (fix bg and redirect_to)

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,3 +1,34 @@
 class RoomsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  before_action :get_curr_hotel, only: [:create,:destroy]
+  
+  def new
+  	@room = Room.new
+  end
+
+  def create
+    #@hotel = Hotel.find(params[:hotel_id])
+    @room = @hotel.rooms.build(room_params)
+    redirect_to hotel_path(@hotel) if @room.save
+  end
+
+  def destroy
+    #@hotel = Hotel.find(params[:hotel_id])
+    @room = @hotel.rooms.find(params[:id])
+    @room.destroy
+    redirect_to hotel_path(@hotel)
+  end
+ 
+  private
+    def room_params
+      params.require(:room).permit(:roomtype, :price, :description)
+    end
+
+
+    def get_curr_hotel 
+      @hotel = Hotel.find(params[:hotel_id])
+    end
 	
 end
