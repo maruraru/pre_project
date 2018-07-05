908b299d35dba056d65f927e5b250c3600e71d73
Avir <qliray@yandex.ru>
2018-07-05 04:28:16 +0300

    m_test

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,22 @@ module PreProject
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    
+
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
+    config.action_mailer.delivery_method = :smtp
+  	config.action_mailer.smtp_settings = {
+    
+    config.action_mailer.smtp_settings = {
+      :address              => 'smtp.gmail.com',
+      :port                 => 587,
+      :domain               => 'gmail.com',
+      :user_name            => 'katarullosan@gmail.com',
+      :password             => 'Kord12A2',
+      :authentication       => :plain,
+      :enable_starttls_auto => true
+  	}
+    
     # config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
